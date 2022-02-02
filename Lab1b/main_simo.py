55767bea47d55a5eb04595512cdec24f60970240
simon <bonato@kth.se>
2022-02-02 10:32:43 +0100

    Classes creation + labels

--- Lab1b/main_simo.py
+++ Lab1b/main_simo.py
@@ -12,12 +12,15 @@ sigma_B = 0.3
 
 classA_1 = multivariate_normal(m_A, [[sigma_A,0],[0, sigma_A]], int(n * 0.5))
 classA_2 = multivariate_normal([-m_A[0],-m_A[1]], [[sigma_A,0],[0, sigma_A]], int(n * 0.5))
+
+classA = np.concatenate((classA_1,classA_2))
 classB = multivariate_normal(m_B, [[sigma_B,0],[0, sigma_B]], n)
 
 
-plt.scatter(classA_1[:,0], classA_1[:,1])
-plt.scatter(classA_2[:,0], classA_2[:,1])
-
+plt.scatter(classA[:,0], classA[:,1])
 plt.scatter(classB[:,0], classB[:,1])
 
+patterns = np.array([[[x[0], x[1], 1] for x in classA] + [[x[0], x[1], -1] for x in classB]])
+
+print(patterns)
 plt.show()
