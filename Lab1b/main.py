bc89927e8121d5f14fb3bfeeb09aa0393df187b6
simon <bonato@kth.se>
2022-02-02 10:47:16 +0100

    Classes creation + labels in function

--- Lab1b/main.py
+++ Lab1b/main.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 
 from numpy.random import multivariate_normal
 
+
 n = 100
 m_A = [1.0, 0.3]
 m_B = [0.0, -0.1]
@@ -10,9 +11,11 @@ m_B = [0.0, -0.1]
 sigma_A = 0.2
 sigma_B = 0.3
 
+bias = 1
+
 def get_patterns():
     # create class A (disjoint) and B, with specified global means and cov (diagonal)
-    # return them with labels 1 and -1
+    # return classes with bias coordinate
     classA_1 = multivariate_normal(m_A, [[sigma_A,0],[0, sigma_A]], int(n * 0.5))
     classA_2 = multivariate_normal([-m_A[0],-m_A[1]], [[sigma_A,0],[0, sigma_A]], int(n * 0.5))
 
@@ -23,5 +26,9 @@ def get_patterns():
     plt.scatter(classA[:,0], classA[:,1])
     plt.scatter(classB[:,0], classB[:,1])
 
-    patterns = np.array([[[x[0], x[1], 1] for x in classA] + [[x[0], x[1], -1] for x in classB]])
-    return patterns
+    patterns = np.array([[[x[0], x[1], bias] for x in classA] + [[x[0], x[1], bias] for x in classB]])
+    targets = np.array([[1 for x in classA] + [-1 for x in classB]])
+
+
+    return patterns, targets
+
