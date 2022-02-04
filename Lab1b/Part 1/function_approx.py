f7683be302dac3566bc5efadda911fa0e8d359be
Luca Marini <lucamarini1998@gmail.com>
2022-02-04 17:15:00 +0100

    add constants and modify plot

--- Lab1b/Part 1/function_approx.py
+++ Lab1b/Part 1/function_approx.py
@@ -4,22 +4,31 @@ import matplotlib.pyplot as plt
 from numpy.random import normal
 from main import forward_pass, backward_pass, weight_update, MSE
 
-HIDDEN_NODES = 3
+HIDDEN_NODES = 25
 EPOCHS = 300
 LEARNING_RATE = 0.001
 N_SAMPLES = 0
 BIAS = 1
 
-def fun(x, y):
+# plot constants
+X_MIN = -5
+X_MAX = -X_MIN
+Y_MIN = X_MIN
+Y_MAX = X_MAX
+Z_MIN = -0.7
+Z_MAX = -Z_MIN
+
+
+def bell_gaussian_func(x, y):
     return np.exp(- (x ** 2 + y ** 2) * 0.1) - 0.5
 
 
-def generate_2d_gaussian(from_xy=-5, to_xy=5.01, step=0.5, n_samples=N_SAMPLES):
+def generate_2d_gaussian(from_xy=-5, to_xy=5.01, step=0.5):
     x = np.arange(from_xy, to_xy, step)
     y = np.arange(from_xy, to_xy, step)
     global N_SAMPLES
     N_SAMPLES = len(x)
-    targets = np.array([[fun(x_elem, y_elem) for x_elem in x] for y_elem in y])
+    targets = np.array([[bell_gaussian_func(x_elem, y_elem) for x_elem in x] for y_elem in y])
     targets = targets.reshape((N_SAMPLES ** 2,))
     [xx, yy] = np.meshgrid(x, y)
     patterns = np.transpose(np.concatenate((xx.reshape(1, N_SAMPLES ** 2), yy.reshape(1, N_SAMPLES ** 2))))
@@ -29,12 +38,14 @@ def generate_2d_gaussian(from_xy=-5, to_xy=5.01, step=0.5, n_samples=N_SAMPLES):
     return patterns, targets
 
 
-def plot_3d(patterns, targets):
+def plot_3d(patterns, targets, i_epoch):
     fig = plt.figure()
-    ax = fig.add_subplot(111, projection='3d')
-    ax.set_xlim(-5, 5)
-    ax.set_ylim(-5, 5)
-    ax.set_zlim(-0.7, 0.7)
+    plt.title("network approximation of function - epoch {}".format(i_epoch + 1))
+    plt.axis('off')
+    ax = fig.add_subplot(projection='3d')
+    ax.set_xlim(X_MIN, X_MAX)
+    ax.set_ylim(Y_MIN, Y_MAX)
+    ax.set_zlim(Z_MIN, Z_MAX)
     patterns_t = np.transpose(patterns)
     X, Y = patterns_t[0], patterns_t[1]
     X = X.reshape((N_SAMPLES, N_SAMPLES))
@@ -44,10 +55,11 @@ def plot_3d(patterns, targets):
     Z = zs.reshape(X.shape)
     ax.plot_surface(X, Y, Z)
 
-    ax.set_xlabel('X Label')
-    ax.set_ylabel('Y Label')
-    ax.set_zlabel('Z Label')
+    ax.set_xlabel('X')
+    ax.set_ylabel('Y')
+    ax.set_zlabel('Z')
     plt.show()
+
 
 def save_errors(o_out, targets, MSE_errors,):
     MSE_errors.append(MSE(o_out, targets))
@@ -67,18 +79,15 @@ def main():
         h_in, h_out, o_in, o_out = forward_pass(patterns, w, v)
         save_errors(o_out, targets, MSE_errors)
 
-        #if i_epoch == EPOCHS - 1:
-        # 3d-plot
-        plot_3d(patterns.transpose(), o_out)
-
-
+        if i_epoch == EPOCHS - 1:
+            # 3d-plot
+            plot_3d(patterns.transpose(), o_out, i_epoch)
 
         print(f"EPOCH {i_epoch:4d} | training_mse = {MSE(o_out, targets):4.2f} |")
 
         delta_h, delta_o = backward_pass(v, targets, h_in, o_out, o_in, HIDDEN_NODES)
         w, dw = weight_update(w, patterns, delta_h, lr=LEARNING_RATE, momentum=False, d_old=dw)
         v, dv = weight_update(v, h_out, delta_o, lr=LEARNING_RATE, momentum=False, d_old=dv)
-
 
 
 if __name__ == '__main__':
