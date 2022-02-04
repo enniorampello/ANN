82f3c32a1b5c5489877e3e18151ead7568d068c4
Luca Marini <lucamarini1998@gmail.com>
2022-02-04 14:26:51 +0100

    3d plot

--- Lab1b/Part 1/function_approx.py
+++ Lab1b/Part 1/function_approx.py
@@ -1,17 +1,49 @@
 import numpy as np
+import matplotlib.pyplot as plt
+from fontTools.misc.plistlib import _array_element
 
-def generate_2d_gaussian(from_xy=-0.5, to_xy=0.5, n_samples=100):
-    x = np.linspace(-0.5, 0.5, n_samples)
-    y = np.linspace(-0.5, 0.5, n_samples)
-    
-    targets = np.subtract(np.exp(-(np.add(np.square(x), np.square(y))/10)), 10)
-    patterns = np.array([(x[i], y[i]) for i in range(len(x))])
+n_samples = 100
+
+
+def fun(x, y):
+    return np.exp(-(x**2 + y**2)*0.1) - 0.5
+
+
+def generate_2d_gaussian(from_xy=-0.5, to_xy=0.5, n_samples=n_samples):
+    x = np.linspace(from_xy, to_xy, n_samples)
+    y = np.linspace(from_xy, to_xy, n_samples)
+    targets = np.array([[fun(x_elem, y_elem) for x_elem in x] for y_elem in y])
+    targets = targets.reshape((n_samples**2, 1))
+    [xx, yy] = np.meshgrid(x, y)
+    patterns = np.transpose(np.concatenate((xx.reshape(1, n_samples**2), yy.reshape(1, n_samples**2))))#np.array([(xx[i], yy[i]) for i in range(len(xx))])
 
     return patterns, targets
-    
+
+
+def plot_3d(patterns, targets):
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    patterns_t = np.transpose(patterns)
+    X, Y = patterns_t[0], patterns_t[1]
+    X = X.reshape((n_samples, n_samples))
+    Y = Y.reshape((n_samples, n_samples))
+
+    zs = targets
+    Z = zs.reshape(X.shape)
+    ax.plot_surface(X, Y, Z)
+
+    ax.set_xlabel('X Label')
+    ax.set_ylabel('Y Label')
+    ax.set_zlabel('Z Label')
+    plt.show()
+
 
 def main():
-    generate_2d_gaussian()
+
+    patterns, targets = generate_2d_gaussian()
+    plot_3d(patterns, targets)
+
 
 if __name__ == '__main__':
     main()
+    
