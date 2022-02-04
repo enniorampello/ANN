4ef53da569dee0c4ea61068049caef3184ceaebf
Luca Marini <lucamarini1998@gmail.com>
2022-02-04 15:18:05 +0100

    adjust samples

--- Lab1b/Part 1/function_approx.py
+++ Lab1b/Part 1/function_approx.py
@@ -7,19 +7,21 @@ from main import forward_pass, backward_pass, weight_update, MSE
 HIDDEN_NODES = 3
 EPOCHS = 30
 LEARNING_RATE = 0.001
-N_SAMPLES = 100
+N_SAMPLES = 0
 
 def fun(x, y):
     return np.exp(- (x ** 2 + y ** 2) * 0.1) - 0.5
 
 
-def generate_2d_gaussian(from_xy=-0.5, to_xy=0.5, n_samples=N_SAMPLES):
-    x = np.linspace(from_xy, to_xy, n_samples)
-    y = np.linspace(from_xy, to_xy, n_samples)
+def generate_2d_gaussian(from_xy=-5, to_xy=5.01, step=0.5, n_samples=N_SAMPLES):
+    x = np.arange(from_xy, to_xy, step)
+    y = np.arange(from_xy, to_xy, step)
+    global N_SAMPLES
+    N_SAMPLES = len(x)
     targets = np.array([[fun(x_elem, y_elem) for x_elem in x] for y_elem in y])
-    targets = targets.reshape((n_samples**2,))
+    targets = targets.reshape((N_SAMPLES**2,))
     [xx, yy] = np.meshgrid(x, y)
-    patterns = np.transpose(np.concatenate((xx.reshape(1, n_samples**2), yy.reshape(1, n_samples**2))))#np.array([(xx[i], yy[i]) for i in range(len(xx))])
+    patterns = np.transpose(np.concatenate((xx.reshape(1, N_SAMPLES**2), yy.reshape(1, N_SAMPLES**2))))
 
     return patterns, targets
 
@@ -27,6 +29,9 @@ def generate_2d_gaussian(from_xy=-0.5, to_xy=0.5, n_samples=N_SAMPLES):
 def plot_3d(patterns, targets):
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
+    ax.set_xlim(-5, 5)
+    ax.set_ylim(-5, 5)
+    ax.set_zlim(-0.7, 0.7)
     patterns_t = np.transpose(patterns)
     X, Y = patterns_t[0], patterns_t[1]
     X = X.reshape((N_SAMPLES, N_SAMPLES))
@@ -59,10 +64,9 @@ def main():
         h_in, h_out, o_in, o_out = forward_pass(patterns, w, v)
         save_errors(o_out, targets, MSE_errors)
 
-        if i_epoch == EPOCHS - 1:
-            # 3d-plot
-
-            plot_3d(patterns.transpose(), o_out)
+        #if i_epoch == EPOCHS - 1:
+        # 3d-plot
+        plot_3d(patterns.transpose(), o_out)
 
 
 
