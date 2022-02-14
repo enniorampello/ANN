da6c5f1e3027b0ee746c599a0d427a1f816a20e5
Luca Marini <lucamarini1998@gmail.com>
2022-02-14 12:23:38 +0100

    load data for 4.3

--- Lab2/4.3/functions.py
+++ Lab2/4.3/functions.py
@@ -5,27 +5,28 @@ def init_weights(size):
     return np.random.random(size=size)
 
 
-def get_votes(path='../4.1/data/votes.dat'):
+def get_votes(path, shape):
     data = np.genfromtxt(path, delimiter=',')
+    data = data.reshape(shape)
     return data
 
 
-def get_parties(path='../4.1/data/mpparty.dat'):
+def get_parties(path):
     party = np.genfromtxt(path, comments="%")
     return party
 
 
-def get_genders(path='../4.1/data/mpsex.dat'):
+def get_genders(path):
     sex = np.genfromtxt(path, comments="%")
     return sex
 
 
-def get_districts(path='../4.1/data/mpdistrict.dat'):
+def get_districts(path):
     districts = np.genfromtxt(path, comments="%")
     return districts
 
 
-def get_names(path='../4.1/data/mpnames.txt'):
+def get_names(path):
     names = np.loadtxt(path, dtype=str, delimiter='\n')
     for i in range(len(names)):
         names[i] = names[i].replace("'", '')
--- Lab2/4.3/main_luca.py
+++ Lab2/4.3/main_luca.py
@@ -1,9 +1,12 @@
 import numpy as np
 from functions import *
 
+# data shapes
+VOTES_SHAPE = (349, 31)
+
 # data paths
 VOTES_PATH = "../4.1/data/votes.dat"
-PARTIES_PATH = "../4.1/data/votes.dat"
+PARTIES_PATH = "../4.1/data/mpparty.dat"
 GENDERS_PATH = "../4.1/data/mpsex.dat"
 DISTRICTS_PATH = "../4.1/data/mpdistrict.dat"
 NAMES_PATH = "../4.1/data/mpnames.txt"
@@ -12,7 +15,10 @@ NAMES_PATH = "../4.1/data/mpnames.txt"
 def main():
     # load data
     # 0 = no-vote; 1 = yes-vote; 0.5 = missing vote
-    votes = get_votes(path=VOTES_PATH)
+    # each row -is one mp; each col is one vote
+    votes = get_votes(VOTES_PATH, VOTES_SHAPE)
+    # each elem of parties, genders, names is the party, gender, name of the correspondent mp
+
     # 0=no party, 1='m', 2='fp', 3='s', 4='v', 5='mp', 6='kd', 7='c'
     parties = get_parties(path=PARTIES_PATH)
     # Male 0, Female 1
