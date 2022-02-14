2fa6194c9a20c9959aa613f64abdba9738f81647
Luca Marini <lucamarini1998@gmail.com>
2022-02-14 12:14:42 +0100

    load data for 4.3

--- Lab2/4.3/main_luca.py
+++ Lab2/4.3/main_luca.py
@@ -11,8 +11,11 @@ NAMES_PATH = "../4.1/data/mpnames.txt"
 
 def main():
     # load data
+    # 0 = no-vote; 1 = yes-vote; 0.5 = missing vote
     votes = get_votes(path=VOTES_PATH)
+    # 0=no party, 1='m', 2='fp', 3='s', 4='v', 5='mp', 6='kd', 7='c'
     parties = get_parties(path=PARTIES_PATH)
+    # Male 0, Female 1
     genders = get_genders(path=GENDERS_PATH)
     districts = get_districts(path=DISTRICTS_PATH)
     names = get_names(path=NAMES_PATH)
