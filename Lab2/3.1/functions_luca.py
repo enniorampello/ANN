98149cb046e7367cc8ac66d3a8b266783c37ab70
Luca Marini <lucamarini1998@gmail.com>
2022-02-15 22:52:05 +0100

    hola chica

---
+++ Lab2/3.1/functions_luca.py
@@ -0,0 +1,328 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
+
+
+def sin(patterns, start=0, stop=2 * np.pi):
+    """
+
+    Args:
+        start (int, optional): starting point of the sequence. Set to 0 for training set and 0.05 for val and np.pi+0.5 for test. Defaults to 0.
+        stop ([type], optional): ending point of the sequence. Only use with test/val. If val, set to np.pi, else 2*np.pi. Defaults to 2*np.pi.
+
+    Returns:
+        np.array: array containing the sequence of points
+    """
+    return np.sin(2 * patterns).reshape(len(patterns), 1)
+
+
+def sin_prime(patterns):
+    return 2 * np.cos(2 * patterns).reshape(len(patterns), 1)
+
+
+def square(patterns, start=0, stop=2 * np.pi):
+    return np.array([1 if np.sin(2 * x) >= 0 else -1 for x in patterns]).reshape(len(patterns), 1)
+
+
+def phi(r, sigma):
+    return np.exp(-(r ** 2) / (2 * sigma ** 2))
+
+
+def init_means(num_nodes, import_data=False, patterns=None):
+    if import_data:
+        kmeans = KMeans(n_clusters=num_nodes, random_state=0).fit(patterns)
+        return kmeans.cluster_centers_
+    else:
+        return np.linspace(0, 2 * np.pi, num_nodes).reshape(num_nodes, 1)
+
+
+def init_weights(num_nodes, import_data=False):
+    if import_data:
+        return np.random.normal(0, scale=1, size=(num_nodes, 2))
+    else:
+        return np.random.normal(0, scale=1, size=(num_nodes, 1))
+
+
+def add_noise(points, sigma):
+    noise = np.random.normal(0, scale=sigma, size=(len(points), 1))
+    return points + noise
+
+
+def train_batch(phi_mat, targets):
+    return np.linalg.inv(phi_mat @ phi_mat.T) @ phi_mat @ targets
+
+
+def residual_error(test_preds, test_targets):
+    test_preds = np.array(test_preds)
+    return (sum(abs(test_preds-test_targets)) / test_preds.shape[0])[0]
+
+def train_seq(patterns, targets, w, max_epochs, sigma, mu, lr, plot, ES, val_patterns, val_targets, patience,
+              import_data):
+
+    if plot and not import_data:
+        pred = [forward_pass(x, mu, w, sigma)[1] for x in patterns]
+        plt.ion()
+        fig = plt.figure()
+        ax = fig.add_subplot(111)
+        line, = ax.plot(patterns, pred)
+        fig.canvas.draw()
+        plt.show(block=False)
+
+    val_errors = []
+    train_errors = []
+    patience_counter = 0
+    for epoch in range(max_epochs):
+        error = 0
+        for pattern, target in zip(patterns, targets):
+            h_out, _ = forward_pass(pattern, mu, w, sigma)
+            w = update_weights(target, h_out, w, lr)
+            error += abs(target - np.sum(h_out * w))
+        error /= patterns.shape[0]
+        train_errors.append(error)
+
+        val_error = 0
+        for pattern, target in zip(val_patterns, val_targets):
+            h_out, val_pred = forward_pass(pattern, mu, w, sigma)
+            val_error += abs(target - np.sum(h_out * w))
+        val_error /= val_patterns.shape[0]
+        val_errors.append(val_error)
+
+        if ES:
+            if epoch > 1 and val_errors[-1][0] > val_errors[-2][0]:
+                patience_counter += 1
+            else:
+                patience_counter = 0
+            if patience_counter >= patience:
+                print(epoch)
+                print('Early stopping!!')
+                return w, train_errors, val_errors, epoch
+
+        #print(f'EPOCH {epoch}\t| error {error[0]} | val error {val_errors[-1][0]}')
+
+        if epoch % 10 == 0 and plot and not import_data:
+            # clear_output(wait=False)
+            pred = [forward_pass(x, mu, w, sigma)[1] for x in patterns]
+            # fig = plt.figure()
+            line.set_xdata(patterns)
+            line.set_ydata(pred)
+            ax.relim()
+            ax.autoscale_view(True, True, True)
+            fig.canvas.draw()
+            plt.pause(0.01)
+    print(epoch)
+    return w, train_errors, val_errors, epoch
+
+
+def forward_pass(pattern, mu, w, sigma):
+
+    h_in = np.abs(mu - pattern)
+
+    if h_in.shape[1] != 1:
+        new_h_in = np.zeros((h_in.shape[0], 1))
+        for row in range(h_in.shape[0]):
+            for col in range(h_in.shape[1]):
+                new_h_in[row] = np.sqrt(np.sum(h_in[row]**2))
+        h_in = new_h_in
+
+    h_out = phi(h_in, sigma)
+    o_out = np.sum(w * h_out, axis=0)
+
+    return h_out, o_out
+
+
+def update_weights(target, h_out, w, lr):
+
+    w += lr * (target - np.sum(h_out * w, axis=0)) * h_out
+    return w
+
+
+def print_function(f, start=0, stop=2 * np.pi):
+    x = np.linspace(start, start + stop, int(stop / 0.1))
+    plt.figure()
+    plt.plot(x, f)
+    plt.show()
+
+
+def euclidean_distance(a, b):
+    return np.linalg.norm(a - b)
+
+
+def get_patterns_indexes(patterns):
+    return [i for i in range(patterns.shape[0])]
+
+
+def get_first_n_argmins(list, n):
+    return np.asarray(list).argsort()[:n]
+
+
+def get_update_of_mean(lr, single_pattern, mu):
+    return lr * (single_pattern - mu)
+
+
+def competitive_learning(patterns, mu, lr_cl, n_nodes, more_winners, n_winners=1, max_epochs=10):
+    for _ in range(max_epochs):
+        patterns_idx = get_patterns_indexes(patterns)
+        # shuffle pattern indexes
+        np.random.shuffle(patterns_idx)
+        for i in range(patterns.shape[0]):
+            # at each iteration of CL a training vector is randomly selected from the data
+            selected_pattern = patterns[patterns_idx[i]]
+            # find the closest RBF unit
+            dist_from_rbf_nodes = []
+            for node_idx in range(n_nodes):
+                dist_from_rbf_nodes.append(euclidean_distance(selected_pattern, mu[node_idx]))
+
+            if more_winners:
+                # strategy to avoid dead units
+                # n closest RBF units - winners
+                nearest_rbf_nodes_idx = get_first_n_argmins(dist_from_rbf_nodes, n_winners)
+                for winner_idx in nearest_rbf_nodes_idx:
+                    mu[winner_idx] += get_update_of_mean(lr_cl, selected_pattern, mu[winner_idx])
+            else:
+                # single closest RBF unit - winner
+                nearest_rbf_node_idx = np.argmin(dist_from_rbf_nodes)
+                mu[nearest_rbf_node_idx] += get_update_of_mean(lr_cl, selected_pattern,
+                                                               mu[nearest_rbf_node_idx])
+
+def title_builder(MLP, batch, cl, num_nodes, max_epochs, lr,
+                  es, patience, lr_cl, epochs_cl, more_winners, test, validation, learning_curves=False):
+    # title builder
+    title = f''
+
+    if test:
+        title += 'Test - '
+    elif validation:
+        title += 'Validation - '
+    elif learning_curves:
+        title += 'Learning curves - '
+    else:
+        title += 'Training - '
+
+    if MLP:
+        title += 'MLP'
+    else:
+        if batch:
+            title += 'RBF batch mode'
+        else:
+            if cl:
+                title += f'RBF seq competitive'
+            else:
+                title += 'RBF seq mode'
+
+    title += f' - hn {num_nodes} - epochs {max_epochs} - lr {lr}'
+
+    if es:
+        title += f'\nes {es} - patience {patience}'
+    if cl:
+        title += f'\nlr cl {lr_cl} - epochs cl {epochs_cl} - more winners {more_winners}'
+    else:
+        title += '\n'
+    return title
+
+def plot_test_results(test_patterns, test_targets, test_preds):
+    # currently, it works only for sine and square function
+    plt.figure()
+    plt.title('test set')
+    plt.plot(test_patterns, test_targets, label='True values - test set')
+    plt.plot(test_patterns, test_preds, label='Predictions - test set')
+    plt.legend()
+    plt.show()
+
+def plot(patterns, targets, preds, lr, num_nodes, max_epochs,
+         batch=False, cl=False, es=False, patience=None, MLP=False,
+         lr_cl=None, epochs_cl=None, more_winners=False,
+         import_data=False, val_patterns=None, val_preds=None,
+         centroids=None, test=False, validation=False, centroids_init=None, competitive=False):
+
+    patterns = np.array(patterns)
+    targets = np.array(targets)
+    preds = np.array(preds)
+
+    val_patterns = np.array(val_patterns)
+    val_preds = np.array(val_preds)
+
+    # angle, velocity
+    # distance, height
+
+    if import_data:
+        for c in range(targets.shape[1]):
+            fig = plt.figure()
+            ax = fig.add_subplot(projection='3d')
+            ax.set_xlabel('Angle')
+            ax.set_ylabel('Velocity')
+
+            if c == 0:
+                ax.set_zlabel('Distance')
+            else:
+                ax.set_zlabel('Height')
+
+            ax.scatter(patterns[:, 0], patterns[:, 1], preds[:, c], label='Preds', c='k')
+            ax.scatter(patterns[:, 0], patterns[:, 1], targets[:, c], label='True')
+            ax.scatter(centroids[:, 0], centroids[:, 1], 0, s=100, marker='x', c='r')
+            # if val_patterns is not None:
+            #     ax.scatter(val_patterns[:, 0], val_patterns[:, 1], val_preds[:, c], label='Validation')
+            plt.title(title_builder(MLP, batch, cl, num_nodes, max_epochs, lr,
+                        es, patience, lr_cl, epochs_cl, more_winners, test, validation))
+            plt.legend()
+            plt.show()
+
+    else:
+        #print(centroids)
+        plt.figure()
+        if validation == False and test == False:
+            plt.scatter(centroids_init, np.zeros(centroids_init.shape), marker='x', c='r', label="initial centroids")
+            if competitive:
+                plt.scatter(centroids, np.zeros(centroids.shape), marker='x', c='black', label="updated centroids")
+        plt.plot(patterns, targets, label='True values')
+        plt.plot(patterns, preds, label='Predictions')
+        # if val_patterns is not None:
+        #     plt.plot(val_patterns, val_preds, label='Validation')
+        plt.xlabel('x')
+        plt.ylabel('y')
+        plt.legend()
+        plt.title(title_builder(MLP, batch, cl, num_nodes, max_epochs, lr,
+                                es, patience, lr_cl, epochs_cl, more_winners, test, validation))
+        plt.show()
+
+
+def train_val_split(data, val_p, col_of_separation_patterns_targets):
+    np.random.shuffle(data)
+    val = data[:int(val_p * data.shape[0]), :]
+    train = data[int(val_p * data.shape[0]):, :]
+    return train[:, :col_of_separation_patterns_targets], train[:, col_of_separation_patterns_targets:], \
+           val[:, :col_of_separation_patterns_targets], val[:, col_of_separation_patterns_targets:]
+
+
+def mse(preds, targets):
+    errors = preds - targets
+    return np.sum(errors ** 2) / len(preds)
+
+
+def get_continuous_predictions(mu, w, sigma, patterns):
+    return [forward_pass(x, mu, w, sigma)[1] for x in patterns]
+
+
+def get_discrete_predictions(mu, w, sigma, patterns):
+    return [1 if forward_pass(x, mu, w, sigma)[1] >= 0 else -1 for x in patterns]
+
+
+def plot_train_val(mse_errors_train, mse_errors_val, ballistic_data, lr, num_nodes, max_epochs,
+         batch=False, cl=False, es=False, patience=None, MLP=False,
+         lr_cl=None, epochs_cl=None, more_winners=False,
+         import_data=False, val_patterns=None, val_preds=None,
+         centroids=None, test=False, validation=False):
+    if ballistic_data:
+        mse_errors_train = np.mean(mse_errors_train, axis=1)
+        mse_errors_val = np.mean(mse_errors_val, axis=1)
+
+    plt.rcParams["figure.figsize"] = [7.00, 3.50]
+    plt.rcParams["figure.autolayout"] = True
+    fig, ax1 = plt.subplots()
+    plt.title(title_builder(MLP, batch, cl, num_nodes, max_epochs, lr, es, patience, lr_cl, epochs_cl, more_winners,
+                            test, validation, learning_curves=True))
+    mse_line, = ax1.plot(mse_errors_train, color='red', label='Training MSE')
+    ax2 = ax1.twinx()
+    mse_line_val, = ax2.plot(mse_errors_val, color='blue', label='Validation MSE')
+    ax2.legend(handles=[mse_line, mse_line_val])
+    fig.tight_layout()
+    plt.show()
---
+++ Lab2/3.1/iterative_main_luca.py
@@ -0,0 +1,197 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from IPython.display import clear_output
+from functions_luca import *
+from functions_MLP import *
+
+'''
+1. Choose a number of nodes for the hidden layer.
+2. Initialise the means of the Gaussians for the hidden nodes (mu).
+3. Initialise the output weights. (normal distribution?) (w)
+3. Start training the network:
+    a. Feed the training set X into the network.
+    b. For each node, compute phi(x(t) - w[i]).
+    c. Compute the output as a weighted sum of the outputs of the hidden nodes.
+'''
+
+LR = 0.01
+
+MAX_EPOCHS = 500
+SIGMA = 0.5  # 0.5
+
+SINE = True
+
+NOISE = False
+SIGMA_NOISE = 0.1
+
+BATCH = False
+ES = False
+PATIENCE = 50
+
+PLOT = False
+
+# MLP params
+MLP_ = False
+
+# competitive learning constants - only if BATCH = False
+MAX_EPOCHS_CL = 10
+COMPETITIVE = True
+# strategy to avoid dead units
+MORE_THAN_ONE_WINNER = False
+# NUM_OF_WINNERS = int(NUM_NODES / 4)
+# learning rate for competitive learning part
+LR_CL = 0.01
+
+BALLISTIC_DATA = True
+# percentage of val set -> only if BALLISTIC_DATA = True
+val_p = 0.2
+
+
+def main(NUM_NODES):
+    if COMPETITIVE:
+        print("CL")
+        if MORE_THAN_ONE_WINNER:
+            print("more than one winner")
+    else:
+        print("NOT CL")
+
+    NUM_OF_WINNERS = int(NUM_NODES / 2)
+    if BALLISTIC_DATA:
+        # data -> ballistic experiments
+        train_data = np.genfromtxt('data/ballist.dat')
+        test_data = np.genfromtxt('data/balltest.dat')
+
+        col_of_separation_patterns_targets = 2
+
+        patterns, targets, val_patterns, val_targets = train_val_split(train_data, val_p,
+                                                                       col_of_separation_patterns_targets)
+        test_patterns = test_data[:, :col_of_separation_patterns_targets]
+        test_targets = test_data[:, col_of_separation_patterns_targets:]
+
+        mu = init_means(NUM_NODES, BALLISTIC_DATA, patterns)
+        w = init_weights(NUM_NODES, BALLISTIC_DATA)
+    else:
+        # data -> sine or square functions
+        patterns = np.linspace(0, 2 * np.pi, int(2 * np.pi / 0.1)).reshape(int(2 * np.pi / 0.1), 1)
+        val_patterns = np.linspace(0.05, np.pi, int(np.pi / 0.1)).reshape(int(np.pi / 0.1), 1)
+        test_patterns = np.linspace(np.pi + 0.05, 2 * np.pi, int(np.pi / 0.1)).reshape(int(np.pi / 0.1), 1)
+
+        if SINE:
+            # sine function
+            targets = sin(patterns)
+            val_targets = sin(val_patterns)
+            test_targets = sin(test_patterns)
+        else:
+            # square function
+            targets = square(patterns)
+            val_targets = square(val_patterns)
+            test_targets = square(test_patterns)
+
+        if NOISE:
+            targets = add_noise(targets, SIGMA_NOISE)
+            val_targets = add_noise(val_targets, SIGMA_NOISE)
+            test_patterns = add_noise(test_patterns, SIGMA_NOISE)
+
+        mu = init_means(NUM_NODES)
+        init_mu = np.copy(mu)
+        w = init_weights(NUM_NODES)
+
+    phi_mat = np.zeros((NUM_NODES, patterns.shape[0]))
+
+    for i in range(NUM_NODES):
+        for j in range(patterns.shape[0]):
+            phi_mat[i][j] = phi(euclidean_distance(mu[i], patterns[j]), SIGMA)
+
+    if MLP_:
+        v_MLP, w_MLP, preds = MLP(np.transpose(patterns), targets.reshape(targets.shape[0]),
+                                  np.transpose(val_patterns), val_targets.reshape(val_targets.shape[0]),
+                                  MAX_EPOCHS, NUM_NODES, LR, ES, PATIENCE)
+        if PLOT:
+            plot(patterns, targets, np.transpose(preds), LR, NUM_NODES, MAX_EPOCHS,
+                 MLP=True, es=ES, patience=PATIENCE)
+
+    elif BATCH:
+        w = train_batch(phi_mat, targets)
+    else:
+        # sequential - on-line
+        if COMPETITIVE:
+            # competitive learning
+            competitive_learning(patterns, mu, LR_CL, NUM_NODES, MORE_THAN_ONE_WINNER, NUM_OF_WINNERS, MAX_EPOCHS_CL)
+
+        w, train_errors, val_errors, epoch = train_seq(patterns, targets, w, MAX_EPOCHS, SIGMA, mu, LR, PLOT, ES,
+                                                       val_patterns,
+                                                       val_targets, PATIENCE, BALLISTIC_DATA)
+        if PLOT:
+            # plot learning curves
+            plot_train_val(train_errors, val_errors, ballistic_data=BALLISTIC_DATA, lr=LR, num_nodes=NUM_NODES,
+                           max_epochs=MAX_EPOCHS,
+                           batch=False, cl=COMPETITIVE, es=ES, patience=PATIENCE, MLP=False,
+                           lr_cl=LR_CL, epochs_cl=MAX_EPOCHS_CL, more_winners=MORE_THAN_ONE_WINNER,
+                           import_data=BALLISTIC_DATA)
+
+    if BALLISTIC_DATA:
+        preds = get_continuous_predictions(mu, w, SIGMA, patterns)
+        test_preds = get_continuous_predictions(mu, w, SIGMA, test_patterns)
+        val_preds = get_continuous_predictions(mu, w, SIGMA, val_patterns)
+    else:
+        # sine or square function
+        if SINE:
+            # sine function
+            preds = get_continuous_predictions(mu, w, SIGMA, patterns)
+            test_preds = get_continuous_predictions(mu, w, SIGMA, test_patterns)
+            val_preds = get_continuous_predictions(mu, w, SIGMA, val_patterns)
+        else:
+            # square function
+            preds = get_discrete_predictions(mu, w, SIGMA, patterns)
+            test_preds = get_discrete_predictions(mu, w, SIGMA, test_patterns)
+            val_preds = get_discrete_predictions(mu, w, SIGMA, val_patterns)
+
+    resid_error_test_set = residual_error(test_preds, test_targets)
+    mse_test_set = mse(test_preds, test_targets)
+
+    # print("Residual test set: {}".format(resid_error_test_set))
+    # print("MSE test set: {}".format(mse_test_set))
+
+    if PLOT:
+        # training set
+        if COMPETITIVE:
+            comp_param = True
+        else:
+            comp_param = False
+        if BALLISTIC_DATA:
+            init_mu = None
+        plot(patterns, targets, preds, LR, NUM_NODES, MAX_EPOCHS,
+             batch=BATCH, cl=COMPETITIVE, lr_cl=LR_CL, es=ES, patience=PATIENCE, epochs_cl=MAX_EPOCHS_CL,
+             more_winners=MORE_THAN_ONE_WINNER, import_data=BALLISTIC_DATA, centroids=mu, validation=False, test=False,
+             centroids_init=init_mu, competitive=comp_param)
+
+        # test set
+        plot(test_patterns, test_targets, test_preds, LR, NUM_NODES, MAX_EPOCHS,
+             batch=BATCH, cl=COMPETITIVE, lr_cl=LR_CL, es=ES, patience=PATIENCE, epochs_cl=MAX_EPOCHS_CL,
+             more_winners=MORE_THAN_ONE_WINNER, import_data=BALLISTIC_DATA, centroids=mu, test=True)
+        '''
+        # # validation set
+        plot(val_patterns, val_targets, val_preds, LR, NUM_NODES, MAX_EPOCHS,
+             batch=BATCH, cl=COMPETITIVE, lr_cl=LR_CL, es=ES, patience=PATIENCE, epochs_cl=MAX_EPOCHS_CL,
+             more_winners=MORE_THAN_ONE_WINNER, import_data=BALLISTIC_DATA, centroids=mu, validation=True)
+        '''
+    return resid_error_test_set, mse_test_set
+
+
+if __name__ == '__main__':
+
+    #np.random.seed(2)
+
+    NUM_NODES_LIST = [20]#, 4, 6, 8, 13, 20]
+    NUM_TRIALS = 10
+
+    for NUM_NODES in NUM_NODES_LIST:
+        # average error on 10 iterations
+        res_error = 0
+        mse_error = 0
+        for i in range(NUM_TRIALS):
+            (res_error_upd, mse_err_upd) = main(NUM_NODES)
+            res_error += res_error_upd
+            mse_error += mse_err_upd
+
+        print(NUM_NODES, res_error / NUM_TRIALS, mse_error / NUM_TRIALS)
