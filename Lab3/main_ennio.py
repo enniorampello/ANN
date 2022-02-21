999feb1279832f902208af593edd6701a18480bd
ennio <enniorampello5@gmail.com>
2022-02-21 15:08:44 +0100

    ue

--- Lab3/main_ennio.py
+++ Lab3/main_ennio.py
@@ -1,23 +1,46 @@
+import itertools
 import numpy as np
+from functions import *
 
 x1 = np.array([-1, -1, 1, -1, 1, -1, -1, 1])
 x2 = np.array([-1, -1, -1, -1, -1, 1, -1, -1])
 x3 = np.array([-1, 1, 1, -1, -1, 1, -1, 1])
-
+# 
 patterns = np.vstack((x1, x2, x3))
 w = patterns.T @ patterns
-print(patterns)
-
-energy_x1 = - w @ x1 @ x1
-energy_x2 = - w @ x2 @ x2
-energy_x3 = - w @ x3 @ x3
-print(energy_x1, energy_x2, energy_x3)
-
+# energy_x1 = - w @ x1 @ x1
+# energy_x2 = - w @ x2 @ x2
+# energy_x3 = - w @ x3 @ x3
+# print(energy_x1, energy_x2, energy_x3)
+# 
 x1d = np.array([1, -1, 1, -1, 1, -1, -1, 1])
 x2d = np.array([1, 1, -1, -1, -1, 1, -1, -1])
 x3d = np.array([1, 1, 1, -1, 1, 1, -1, 1])
 
-energy_x1d = - w @ x1d @ x1d
-energy_x2d = - w @ x2d @ x2d
-energy_x3d = - w @ x3d @ x3d
-print(energy_x1d, energy_x2d, energy_x3d)
+print(x1)
+print(synch_update(x1d, w))
+print(x2)
+print(synch_update(x2d, w))
+print(x3)
+print(synch_update(x3d, w))
+
+# patterns = itertools.product((-1, 1), repeat=8)
+# count = 0
+# for i, pattern in enumerate(patterns):
+#     if (pattern == synch_update(pattern, w)).all():
+#         count += 1
+# print(count)
+
+# x2dd = np.array([1, 1, 1, 1, 1, 1, -1, -1])
+# print(x2)
+# print(synch_update(x2dd, w))
+
+# energy_x1d = asynch_update(x1d, w, energy=True)
+# energy_x2d = asynch_update(x2d, w, energy=True)
+# energy_x3d = asynch_update(x3d, w, energy=True)
+# print(energy_x1d, energy_x2d, energy_x3d)
+
+# w = np.random.randn(8, 8)
+# w = 0.5 * (w + w.T)
+# x_rand = np.random.choice([-1, 1], size=8)
+# asynch_update(x_rand, w, energy=True)
